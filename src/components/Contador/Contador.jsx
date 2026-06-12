757421fab06594bc7bdf9b1824f67bb57059e71d
Ana Laura <ana.machioni@aluno.senai.br>
2026-06-12 13:48:00 -0300

    add: Fazendo o contador jsx

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -4,25 +4,34 @@ import styles from "./Contador.module.css"
 function Contador() {
 
     const [numero, setNumero] = useState(0)
-    const [numero2, setNumero2] = useState(0)
 
-    const atualizarNumero = () => {
-
+    const aumentarnumero = () => {
         setNumero(numero + 1)
     }
 
+    const subtrairnumero = () => {
+        setNumero(numero - 1)
+    }
 
-    return(
+    const reiniciarnumero = () => {
+        setNumero(0)
+    }
+
+    const sortearnumero = () => {
+        Math.floor(Math.random() * 100);
+    }
+
+    return (
         <div className={styles.contador}>
-            <h1>Contador</h1>
+            <h1>Esse é o contador!</h1>
             <span className={styles.span}>{numero}</span>
-        <div className={styles.buttonContainer}>
-            <button className={styles.button}>-</button>
-            <button className={styles.button}>+</button>
-            <button className={styles.button}>reiniciar</button>
+            <div className={styles.botoesContainer}>
+                <button className={styles.button} onClick={aumentarnumero}>+</button>
+                <button className={styles.button} onClick={subtrairnumero}>-</button>
+                <button className={styles.button} onClick={reiniciarnumero}>Reiniciar</button>
+                <button className={styles.button} onClick={sortearnumero}>Sortear</button>
             </div>
         </div>
-        
     )
 }
 
